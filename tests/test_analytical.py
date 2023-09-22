0f56d6f5236457f6e874af51492f2385358a2577
David Berger <david.berger@tum.de>
2023-09-22 20:25:30 +0200

    Fix test for new TrapDiffusion interface

--- tests/test_analytical.py
+++ tests/test_analytical.py
@@ -1,10 +1,11 @@
 def test_concentration_constant():
     import numpy as np
-    from models.analytical.trapdiffusion import SingleOccupationSingleIsotope
+    from models.analytical import SingleOccupationSingleIsotope
+
     for _ in range(100):
-        analytical_model = SingleOccupationSingleIsotope(2,1)
-        analytical_model.solve(analytical_model.c)
-        corrected = analytical_model.sol.y
-        corrected *= analytical_model.c_S_T[:,None]
-        total = np.sum(corrected, axis=0)
-        assert np.allclose(total - 1,0)
+        analytical_model = SingleOccupationSingleIsotope(2, 1)
+        c0 = analytical_model.initial_values()
+        ts, cs = analytical_model.solve(c0, 50)
+        cs *= analytical_model.c_S_T[:, None]
+        total = np.sum(cs, axis=0)
+        assert np.allclose(total - 1, 0)
