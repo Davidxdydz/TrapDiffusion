320c92d0e6938bd169eb55b63a506a682d63773b
David Berger <david.berger@tum.de>
2023-09-23 19:44:41 +0200

    Add test for MOMI

--- tests/test_analytical.py
+++ tests/test_analytical.py
@@ -1,11 +1,17 @@
 def test_concentration_constant():
     import numpy as np
-    from models.analytical import SingleOccupationSingleIsotope
+    from models.analytical import (
+        SingleOccupationSingleIsotope,
+        MultiOccupationMultiIsotope,
+    )
 
-    for _ in range(100):
-        analytical_model = SingleOccupationSingleIsotope(2, 1)
-        c0 = analytical_model.initial_values()
-        ts, cs = analytical_model.solve(c0, 50)
-        cs *= analytical_model.c_S_T[:, None]
-        total = np.sum(cs, axis=0)
-        assert np.allclose(total - 1, 0)
+    for model in [SingleOccupationSingleIsotope, MultiOccupationMultiIsotope]:
+        for _ in range(100):
+            analytical_model = model()
+            c0 = analytical_model.initial_values()
+            ts, cs = analytical_model.solve(c0)
+            cs *= analytical_model.correction_factors()[:, None]
+            total = np.sum(cs, axis=0)
+            assert np.allclose(
+                total, 1
+            ), f"{analytical_model.name} does not conserve mass"
