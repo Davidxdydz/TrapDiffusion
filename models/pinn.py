7006971196ca971909fc56ca88df14b4d810931e
David Berger <david.berger@tum.de>
2023-09-23 21:17:28 +0200

    Add MOMI pinn

--- models/pinn.py
+++ models/pinn.py
@@ -1,6 +1,6 @@
 import pathlib
 import yaml
-from models.analytical import SingleOccupationSingleIsotope
+from models.analytical import SingleOccupationSingleIsotope, MultiOccupationMultiIsotope
 from models.datasets import load_dataset
 import numpy as np
 import os
@@ -20,7 +20,7 @@ def load_dataset_info(dataset_name, dataset_dir):
     return info
 
 
-def prepare_SISO_fixed(
+def prepare_SOSI_fixed(
     physics_weight=0,
     dataset_name="Single-Occupation, Single Isotope, fixed matrix",
     dataset_dir="datasets",
@@ -55,7 +55,7 @@ def prepare_SISO_fixed(
     return model, physics_loss, (x, y)
 
 
-def prepare_SISO_random(
+def prepare_SOSI_random(
     physics_weight=0,
     dataset_name="Single-Occupation, Single Isotope, random matrix",
     dataset_dir="datasets",
@@ -85,6 +85,39 @@ def prepare_SISO_random(
         corrections = y_true[:, -3:]
         return keras.ops.mean(
             keras.ops.abs(ys - y_pred), axis=0
+        ) + physics_weight * keras.ops.abs(1 - y_pred * corrections)
+
+    return model, physics_loss, (x, y)
+
+
+def prepare_MOMI_fixed(
+    physics_weight=0,
+    dataset_name="Multi-Occupation, Multi Isotope, fixed matrix",
+    dataset_dir="datasets",
+):
+    x, y, info = load_dataset(dataset_name, dataset_dir)
+
+    input_shape = (info["input_dim"],)
+
+    model = keras.Sequential(
+        [
+            keras.layers.Input(shape=input_shape),
+            keras.layers.Dense(512, activation="relu"),
+            keras.layers.Dense(64, activation="relu"),
+            keras.layers.Dense(12, activation="linear"),
+        ],
+        name=f"{dataset_name}, physics_weight={physics_weight}",
+    )
+    corrections = keras.ops.convert_to_tensor(
+        MultiOccupationMultiIsotope().correction_factors()
+    )
+
+    def physics_loss(y_true, y_pred):
+        """
+        Loss function is mae + physics_weight * physics_loss
+        """
+        return keras.ops.mean(
+            keras.ops.abs(y_true - y_pred), axis=0
         ) + physics_weight * keras.ops.abs(1 - y_pred * corrections)
 
     return model, physics_loss, (x, y)
