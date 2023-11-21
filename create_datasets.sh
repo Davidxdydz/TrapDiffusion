db41200426f8edb297e8550265a57b463deca434
David Berger <david.p.berger@gmx.de>
2023-11-21 16:49:53 +0100

    Add slurm submit files

---
+++ create_datasets.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -l
+# Standard output and error:
+#SBATCH -o ./jobs/job.out.%j
+#SBATCH -e ./jobs/job.err.%j
+# Initial working directory:
+#SBATCH -D /raven/u/beda/Source/TrapDiffusion/
+# Job name
+#SBATCH -J create_trap_diffusion_datasets
+#
+#SBATCH --ntasks=1
+#
+#SBATCH --mail-type=none
+#SBATCH --mail-user=david.berger@tum.de
+#SBATCH --time=01:00:00
+
+module purge
+module load anaconda/3/2023.03
+module load pytorch/gpu-cuda-11.6/2.1.0
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+
+source venv/bin/activate
+python create_dataset.py --preset SOSI_fixed
+python create_dataset.py --preset SOSI_random
+python create_dataset.py --preset MOMI_fixed
+nvidia-smi
+deactivate
---
+++ run_search.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+# Standard output and error:
+#SBATCH -o ./jobs/job.out.%j
+#SBATCH -e ./jobs/job.err.%j
+# Initial working directory:
+#SBATCH -D /raven/u/beda/Source/TrapDiffusion/
+# Job name
+#SBATCH -J trap_diffusion
+#
+#SBATCH --ntasks=1
+#SBATCH --constraint="gpu"
+#
+# --- default case: use a single GPU on a shared node ---
+#SBATCH --gres=gpu:a100:1
+#SBATCH --cpus-per-task=18
+#SBATCH --mem=125000
+#
+#SBATCH --mail-type=none
+#SBATCH --mail-user=david.berger@tum.de
+#SBATCH --time=00:01:00
+
+module purge
+module load anaconda/3/2023.03
+module load pytorch/gpu-cuda-11.6/2.1.0
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+
+source venv/bin/activate
+python test.py
+nvidia-smi
+deactivate
