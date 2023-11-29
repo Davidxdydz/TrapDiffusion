6a5b64da2f6d6fef85253d01fd8665dfc24100b7
David Berger <david.p.berger@gmx.de>
2023-11-29 20:14:00 +0100

    Log to logs, parallel execution

--- create_datasets.sh
+++ create_datasets.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 # Standard output and error:
-#SBATCH -o ./jobs/job.out.%j
-#SBATCH -e ./jobs/job.err.%j
+#SBATCH -o ./logs/job.out.%j
+#SBATCH -e ./logs/job.err.%j
 # Initial working directory:
 #SBATCH -D /raven/u/beda/Source/TrapDiffusion/
 # Job name
 #SBATCH -J create_trap_diffusion_datasets
 #
 #SBATCH --ntasks=3
-#SBATCH --mem=10000
+#SBATCH --mem=30000
 #
 #SBATCH --mail-type=none
 #SBATCH --mail-user=david.berger@tum.de
@@ -19,10 +19,11 @@ module load anaconda/3/2023.03
 module load pytorch/gpu-cuda-11.6/2.1.0
 
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
+export PYTHONUNBUFFERED=1
 
 source venv/bin/activate
-srun --ntasks=1 python create_dataset.py --preset SOSI_fixed --quiet &
-srun --ntasks=1 python create_dataset.py --preset SOSI_random --quiet &
-srun --ntasks=1 python create_dataset.py --preset MOMI_fixed --quiet &
+srun --exclusive --ntasks=1 --mem=10000 python create_dataset.py --preset SOSI_fixed --quiet &
+srun --exclusive --ntasks=1 --mem=10000 python create_dataset.py --preset SOSI_random --quiet &
+srun --exclusive --ntasks=1 --mem=10000 python create_dataset.py --preset MOMI_fixed --quiet &
 wait
 deactivate
