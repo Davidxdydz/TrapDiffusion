3815859e4e9cf6ba30f9a6dc91d7e2553bddb162
David Berger <david.p.berger@gmx.de>
2023-11-29 20:00:44 +0100

    Add clear option

--- run_random_search.py
+++ run_random_search.py
@@ -1,5 +1,6 @@
 from models.pinn import SOSIFixed
 import pathlib
+import shutil
 from training.utils import (
     SearchModelGenerator,
     random_search,
@@ -20,18 +21,25 @@ if __name__ == "__main__":
     parser.add_argument("--n", type=int, default=100)
     parser.add_argument("--quiet", "-q", action="store_true", default=False)
     parser.add_argument("--output", "-o", default="random_search/15µs_leaky_relu")
+    parser.add_argument("--clear", "-c", action="store_true", default=False)
     args = parser.parse_args()
     output_dir = pathlib.Path(args.output)
+    compressed_file = output_dir.with_suffix(".pkl.gz")
     sosi_fixed_generator = SearchModelGenerator(
         SOSIFixed(),
         layer_count=ParameterRange([1, 4]),
         layer_sizes=ParameterRange([16, 32, 64, 128, 256]),
         activations=ParameterRange(["relu", "tanh"]),
         output_activation=ParameterRange(["leaky_relu"]),
-        physics_weight=ParameterRange([0, 0.2]),
+        physics_weight=ParameterRange([0, 1], dtype=float),
         epochs=ParameterRange([20, 40]),
         reject=reject,
     )
+    if args.clear:
+        if output_dir.exists():
+            shutil.rmtree(output_dir)
+        compressed_file.unlink(missing_ok=True)
+
     random_search(
         sosi_fixed_generator,
         args.n,
@@ -40,6 +48,6 @@ if __name__ == "__main__":
     )
     compress_random_search(
         output_dir,
-        str(output_dir) + "pkl.gz",
+        compressed_file,
         quiet=True,
     )
