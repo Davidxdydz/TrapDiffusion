f08fabfc97bb0628ee9fdbf696c429228c7411bb
David Berger <david.p.berger@gmx.de>
2023-12-04 18:12:54 +0100

    Rewrite CPUModel for normalization support

--- training/utils.py
+++ training/utils.py
@@ -2,6 +2,7 @@ from tqdm.auto import tqdm
 import yaml
 import pathlib
 import os
+
 os.environ["KERAS_BACKEND"] = "torch"
 import keras
 import random
@@ -11,42 +12,66 @@ import time
 from training.datasets import load_dataset_info, load_dataset
 from dataclasses import dataclass, asdict
 from typing import List
-from models.pinn import ModelBuilder
+from models.pinn import ModelBuilder, Normalizer
+
+
+class CPUNormalizer:
+    def __call__(self, inputs):
+        sums = np.sum(inputs, axis=1, keepdims=True)
+        return inputs / sums
+
+
+class CPUDense:
+    @staticmethod
+    def from_keras(layer: keras.layers.Dense):
+        weights = layer.get_weights()[0]
+        biases = layer.get_weights()[1]
+        activation = layer.activation.__name__
+        return CPUDense(weights, biases, activation)
+
+    def __init__(self, weights, biases, activation):
+        self.weights = weights
+        self.biases = biases
+        self.activation = activation
+
+    def __call__(self, inputs):
+        x = inputs @ self.weights + self.biases
+        if self.activation == "relu":
+            x[x < 0] = 0
+        elif self.activation == "linear":
+            ...
+        elif self.activation == "tanh":
+            x = np.tanh(x)
+        elif self.activation == "leaky_relu":
+            x[x < 0] = 0.3 * x[x < 0]
+        elif self.activation == "sigmoid":
+            x = 1 / (1 + np.exp(-x))
+        else:
+            raise NotImplementedError(f"Activation {self.activation} not implemented")
+        return x
 
 
 class CPUModel:
-    @staticmethod
-    def extract(pinn: keras.Sequential):
-        weights = []
-        biases = []
-        activations = []
+    def get_from(self, pinn: keras.Sequential):
+        layers = []
         for layer in pinn.layers:
-            weights.append(layer.get_weights()[0])
-            biases.append(layer.get_weights()[1])
-            activations.append(layer.activation.__name__)
-        return weights, biases, activations
+            if isinstance(layer, keras.layers.Dense):
+                layers.append(CPUDense.from_keras(layer))
+            elif isinstance(layer, Normalizer):
+                layers.append(CPUNormalizer())
+            else:
+                raise NotImplementedError(f"Layer {layer} not implemented")
+        self.layers = layers
 
     def __init__(self, model: keras.Sequential):
-        self.weights, self.biases, self.activations = CPUModel.extract(model)
+        self.get_from(model)
         self.input_shape = model.input_shape
         self.output_shape = model.output_shape
         self.name = model.name
 
     def predict(self, x):
-        for layer, bias, activation in zip(self.weights, self.biases, self.activations):
-            x = x @ layer + bias
-            if activation == "relu":
-                x[x < 0] = 0
-            elif activation == "linear":
-                ...
-            elif activation == "tanh":
-                x = np.tanh(x)
-            elif activation == "leaky_relu":
-                x[x < 0] = 0.3 * x[x < 0]
-            elif activation == "sigmoid":
-                x = 1 / (1 + np.exp(-x))
-            else:
-                raise NotImplementedError(f"Activation {activation} not implemented")
+        for layer in self.layers:
+            x = layer(x)
         return x
 
 
@@ -244,15 +269,19 @@ class SearchModel:
             callbacks=[scheduler, early_stopping],
             verbose=0 if quiet else 1,
         )
+        info = load_dataset_info(self.dataset_name, self.dataset_dir)
 
         predictions = self.model.predict(x_val, batch_size=2**12, verbose=0)
         maes = np.mean(np.abs(predictions - y_val), axis=1)
         self.max_mae = float(np.max(maes))
         self.mean_mae = float(np.mean(maes))
         self.total_mae = float(np.sum(maes))
-        output_dim = y_val.shape[1]
-        correction_factors = y_train[:, -output_dim:]
-        mass_loss = 1 - np.sum(predictions * correction_factors, axis=1)
+        if not info.get("pre_normalized", False):
+            output_dim = y_val.shape[-1]
+            correction_factors = y_train[:, -output_dim:]
+            mass_loss = 1 - np.sum(predictions * correction_factors, axis=1)
+        else:
+            mass_loss = 1 - np.sum(predictions, axis=1)
         self.mass_loss_mean = np.mean(mass_loss)
         self.mass_loss_std = np.std(mass_loss)
         self.save_name = time.strftime("%Y-%m-%d-%H-%M-%S")
