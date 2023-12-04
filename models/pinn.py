eefe55d7df2e40e9fad51d9406744009be575543
David Berger <david.p.berger@gmx.de>
2023-12-04 18:13:18 +0100

    Add support for training on pre_normalized dataset

--- models/pinn.py
+++ models/pinn.py
@@ -6,6 +6,35 @@ os.environ["KERAS_BACKEND"] = "torch"
 import keras
 
 
+class Normalizer(keras.layers.Layer):
+    def call(self, inputs):
+        sums = keras.ops.sum(inputs, axis=1, keepdims=True)
+        return inputs / sums
+
+
+class PhysicsLoss:
+    def __init__(self, physics_weight):
+        self.physics_weight = physics_weight
+
+    def __call__(self, y_true: keras.KerasTensor, y_pred: keras.KerasTensor):
+        """
+        Loss function is mae + physics_weight * physics_loss
+        """
+        # the y_true also store the correction factors in the second half
+        corrections_start = y_pred.shape[-1]
+        y_true, corrections = (
+            y_true[:, :-corrections_start],
+            y_true[:, -corrections_start:],
+        )
+        mae_loss = keras.losses.mean_absolute_error(y_true, y_pred)
+        total_mass = keras.ops.sum(y_pred * corrections, axis=-1, keepdims=True)
+        physics_loss = keras.ops.abs(1 - total_mass)
+        return (1 - self.physics_weight) * mae_loss + self.physics_weight * physics_loss
+
+    def get_config(self):
+        return dict(physics_weight=self.physics_weight)
+
+
 class ModelBuilder:
     def __init__(self, dataset_name, dataset_dir="datasets"):
         self.dataset_name = dataset_name
@@ -19,12 +48,15 @@ class ModelBuilder:
         activations,
         output_activation,
         name,
+        normalizer=False,
     ):
         model = keras.Sequential(name=name)
         model.add(keras.layers.Input(shape=(intput_channels,)))
         for size, activation in zip(layer_sizes, activations):
             model.add(keras.layers.Dense(size, activation=activation))
         model.add(keras.layers.Dense(output_channels, activation=output_activation))
+        if normalizer:
+            model.add(Normalizer())
         return model
 
     def info(self):
@@ -36,8 +68,18 @@ class ModelBuilder:
     def prepare(self, layer_sizes, activations, physics_weight, output_activation):
         x, y, c, info = load_dataset(self.dataset_name, self.dataset_dir)
 
-        # append the correction factors to the labels, to be used in the loss function
-        y_with_corrections = np.append(y, c, axis=1)
+        x_train = x
+        x_val = x
+        y_train = None
+        y_val = None
+        pre_normalized = info.get("pre_normalized", False)
+        if not pre_normalized:
+            # append the correction factors to the labels, to be used in the loss function
+            y_train = np.append(y, c, axis=1)
+            y_val = y
+        else:
+            y_train = y
+            y_val = y
 
         model = self.build_model(
             intput_channels=info["input_dim"],
@@ -45,18 +87,19 @@ class ModelBuilder:
             layer_sizes=layer_sizes,
             activations=activations,
             output_activation=output_activation,
-            name=f"{self.dataset_name}, physics_weight={physics_weight}",
+            name=f"{self.dataset_name}, {f'physics_weight={physics_weight}' if not pre_normalized else 'pre_normalized'}",
+            normalizer=pre_normalized,
         )
 
-        def physics_loss(y_true, y_pred):
-            """
-            Loss function is mae + physics_weight * physics_loss
-            """
-            ys = y_true[:, : -info["output_dim"]]
-            corrections = y_true[:, -info["output_dim"] :]
-            mae_loss = keras.ops.mean(keras.ops.abs(ys - y_pred), axis=1)
-            total_mass = keras.ops.sum(y_pred * corrections, axis=1, keepdims=True)
-            physics_loss = keras.ops.abs(1 - total_mass)
-            return (1 - physics_weight) * mae_loss + physics_weight * physics_loss
+        loss = (
+            PhysicsLoss(physics_weight)
+            if not pre_normalized
+            else keras.losses.mean_absolute_error
+        )
 
-        return model, physics_loss, (x, y_with_corrections), (x, y)
+        return (
+            model,
+            loss,
+            (x_train, y_train),
+            (x_val, y_val),
+        )
