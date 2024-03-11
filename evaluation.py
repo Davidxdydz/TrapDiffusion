1de4055bc0743b43d88b9f9a724e83df0f730162
David Berger <david.p.berger@gmx.de>
2024-03-11 16:51:21 +0100

    Move search utilities to evaluation

--- evaluation.py
+++ evaluation.py
@@ -5,35 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from models.cpu import CPUSequential
 import os
-from training.utils import SearchModel
 import pandas as pd
 import itertools
-
-
-def load_random_search(directory, quiet=False) -> pd.DataFrame:
-    models = []
-    for dir in tqdm(os.listdir(directory), disable=quiet):
-        path = os.path.join(directory, dir)
-        if os.path.isdir(path):
-            model = SearchModel.load(path)
-            models.append(model)
-    df = pd.DataFrame.from_dict(map(lambda x: x.info(), models))
-    df["max_layer_size"] = df["layer_sizes"].map(max)
-    for i in range(1, 6):
-        df[f"layer_{i}_size"] = df["layer_sizes"].map(
-            lambda x: x[i - 1] if len(x) >= i else None
-        )
-        df[f"layer_{i}_activation"] = df["activations"].map(
-            lambda x: x[i - 1] if len(x) >= i else f"no layer {i}"
-        )
-    return df
-
-
-def compress_random_search(
-    directory: os.PathLike, output_file: os.PathLike, quiet=False
-):
-    df = load_random_search(directory, quiet=quiet)
-    df.to_pickle(output_file, compression="gzip")
 
 
 def plot_color_legend(color_dict, title=None):
@@ -120,3 +93,103 @@ def performance_batch_size_plot(analytical_model: type[TrapDiffusion], surrogate
     plt.xlabel("Batch size")
     plt.grid()
     plt.ylabel(f"Time per prediction [{unit}]")
+
+
+import os
+
+os.environ["KERAS_BACKEND"] = "torch"
+import keras_tuner as kt
+from training.datasets import load_dataset_info
+from models.gpu.pinn import HyperModel
+from pathlib import Path
+import json
+import pandas as pd
+from training.utils import estimate_cpu_time
+from tqdm.auto import tqdm
+import keras
+
+
+def create_tuner(
+    n,
+    output_name,
+    output_dir,
+    clear,
+    max_time,
+    method,
+    dataset_name,
+    dataset_dir,
+):
+    tuner_classes = {
+        "random": kt.tuners.RandomSearch,
+        "bayesian": kt.tuners.BayesianOptimization,
+        "hyperband": kt.tuners.Hyperband,
+    }
+    specifice_args = {
+        "random": {"max_trials": n},
+        "bayesian": {"max_trials": n},
+        "hyperband": {"max_epochs": 30, "hyperband_iterations": n},
+    }
+    info = load_dataset_info(dataset_name, dataset_dir)
+    tuner = tuner_classes[method](
+        hypermodel=HyperModel(
+            input_channels=info["input_channels"],
+            output_channels=info["output_channels"],
+            normalizer=info.get("pre_normalized", False),
+            max_cpu_time=max_time,
+            dataset_name=dataset_name,
+            dataset_dir=dataset_dir,
+        ),
+        objective=kt.Objective("val_max_ae", "min"),
+        overwrite=clear,
+        directory=output_dir,
+        project_name=output_name,
+        **specifice_args[method],
+    )
+    return tuner
+
+
+def load_tuner(path):
+    args_path = Path(path) / "tuner_args.json"
+    tuner_args = json.loads(args_path.read_text())
+    return create_tuner(**tuner_args)
+
+
+def load_model(path, trial_id, reload=False):
+    path = Path(path)
+    model_path = path / f"trial_{trial_id}" / "model.keras"
+    if model_path.exists() and not reload:
+        return keras.models.load_model(model_path)
+    tuner = load_tuner(path)
+    trial = tuner.oracle.get_trial(trial_id)
+    model = tuner.load_model(trial)
+    model.save(model_path)
+
+
+def load_search(path, reload=False):
+    metrics_path = Path(path) / "metrics.pickle"
+    if metrics_path.exists() and not reload:
+        return pd.read_pickle(metrics_path)
+    tuner = load_tuner(path)
+    trials = tuner.oracle.trials
+    data = {}
+    for id, trial in tqdm(trials.items()):
+        all_values = {}
+        try:
+            all_values.update(trial.hyperparameters.values)
+            for metric, history in trial.metrics.metrics.items():
+                all_values[metric] = history.get_last_value()
+            modelpath = Path(path) / f"trial_{id}" / "model.keras"
+            if modelpath.exists() and not reload:
+                model = keras.models.load_model(modelpath)
+            else:
+                model = tuner.load_model(trial)
+                model.save(modelpath)
+            all_values["cpu_time"] = estimate_cpu_time(model)
+            all_values["param_count"] = model.count_params()
+            data[id] = all_values
+        except Exception as e:
+            print(f"Error loading trial {id}: {e}")
+
+    df = pd.DataFrame(data).T.convert_dtypes()
+    df.to_pickle(metrics_path)
+    return df
