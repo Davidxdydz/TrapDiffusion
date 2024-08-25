776dccb0c02c928a468a964a6f5f1a88d2eb0ff7
David Berger <david.berger@tum.de>
2024-08-25 14:59:17 +0200

    Add MOMI hypermodel

--- models/gpu/pinn.py
+++ models/gpu/pinn.py
@@ -5,12 +5,13 @@ import keras
 import keras_tuner as kt
 from training.utils import estimate_cpu_time
 from keras import ops
-from models.gpu.layers import Normalizer
+from models.gpu.layers import Normalizer, IsotopeNormalizer
 from models.gpu.metrics import (
     MaxAbsoluteError,
     MeanAbsoluteMassLoss,
     cut_labels,
 )
+from training.utils import CustomTensorboard
 
 
 class PhysicsLoss(keras.losses.Loss):
@@ -77,7 +78,7 @@ class HyperModel(kt.HyperModel):
             model.add(Normalizer(norm_loss_weight=norm_loss_weight))
             loss_function = "mae"
         else:
-            physics_weight = hp.Float("physics_weight", 0.4, 1)
+            physics_weight = hp.Float("physics_weight", 0, 1)
             loss_function = PhysicsLoss(physics_weight=physics_weight)
 
         learning_rate = hp.Float("learning_rate", 1e-4, 1e-3, sampling="log")
@@ -103,5 +104,109 @@ class HyperModel(kt.HyperModel):
             *args,
             shuffle=True,
             batch_size=2**12,
+            **kwargs,
+        )
+
+
+class MIMOHyperModel(kt.HyperModel):
+    n = 0
+
+    def __init__(
+        self,
+        input_channels,
+        output_channels,
+        normalizer=False,
+        retries=50,
+        dataset_name=None,
+        dataset_dir="datasets",
+        **kwargs,
+    ):
+        super(kt.HyperModel, self).__init__(**kwargs)
+        self.input_channels = input_channels
+        self.output_channels = output_channels
+        self.retries = retries
+        self.dataset_name = dataset_name
+        self.dataset_dir = dataset_dir
+
+    def create_model(self, hp: kt.HyperParameters):
+        mlp = keras.Sequential(name="unnormed")
+        mlp.add(keras.layers.Input(shape=(self.input_channels,)))
+        activations = {
+            0: ["tanh", "leaky_relu"],
+            1: ["relu", "leaky_relu"],
+            2: ["leaky_relu", "sigmoid"],
+            3: ["tanh", "leaky_relu"],
+            4: ["relu", "sigmoid", "leaky_relu"],
+            5: ["sigmoid"],
+        }
+        for i in range(hp.Int("num_layers", 5, 5)):
+            mlp.add(
+                keras.layers.Dense(
+                    units=hp.Int(f"layer_size_{i}", 32, 900, 32),
+                    activation=hp.Choice(f"activation_{i}", activations[i]),
+                )
+            )
+        mlp.add(
+            keras.layers.Dense(
+                units=self.output_channels,
+                activation=hp.Choice("output_activation", ["leaky_relu", "sigmoid"]),
+            )
+        )
+        normed_loss_weight = hp.Float("normed_weight", 0, 0.5)
+        unnormed_loss_weight = 1 - normed_loss_weight
+        ratio_normalizer = IsotopeNormalizer(name="normed")
+        inputs = keras.layers.Input(shape=(self.input_channels,))
+        c = ops.abs(ops.transpose(inputs[..., 1:-9]))
+        matrix = ops.array(
+            # [c_H, c_D, c_T_00, c_T_10, c_T_01, c_T_20, c_T_11, c_T_02, c_T_30, c_T_21, c_T_12, c_T_03]
+            [
+                [1, 0, 0, 1, 0, 2, 1, 0, 3, 2, 1, 0],
+                [0, 1, 0, 0, 1, 0, 1, 2, 0, 1, 2, 3],
+            ],
+            dtype=float,
+        )
+        tmp = matrix @ c
+        ratios = tmp[0] / tmp[1]
+        unnormed = mlp(inputs)
+        normed = ratio_normalizer(unnormed, ratios)
+        model = keras.Model(
+            inputs=inputs,
+            outputs={"normed": normed, "unnormed": unnormed},
+            name=f"trial_{MIMOHyperModel.n}",
+        )
+        MIMOHyperModel.n += 1
+        model.compile(
+            optimizer=keras.optimizers.Adam(
+                learning_rate=hp.Float("learning_rate", 1e-3, 1e-3, sampling="log")
+            ),
+            loss={
+                "unnormed": hp.Choice("unnormed_loss", ["mse", "mae"]),
+                "normed": hp.Choice("normed_loss", ["mae"]),
+            },
+            metrics={
+                "unnormed": MaxAbsoluteError(),
+                "normed": MaxAbsoluteError(),
+            },
+            loss_weights={
+                "unnormed": unnormed_loss_weight,
+                "normed": normed_loss_weight,
+            },
+        )
+        return model
+
+    def build(self, hp: kt.HyperParameters):
+        return self.create_model(hp)
+
+    def fit(self, hp: kt.HyperParameters, model, *args, **kwargs):
+        # kwargs.get("callbacks", []).append(
+        #     keras.callbacks.ReduceLROnPlateau(factor=0.5, min_lr=1e-4)
+        # )
+        kwargs.get("callbacks", []).append(
+            CustomTensorboard(log_dir="logs/momi3", name=model.name)
+        )
+        return model.fit(
+            *args,
+            shuffle=True,
+            batch_size=2**14,
             **kwargs,
         )
